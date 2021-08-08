5b4f69a7341d05fd9d640b6d602392228b3d62e3
MNoxx74 <ampam877@gmail.com>
2021-08-08 09:29:26 +0700

    Make the TWRP Unified

--- omni_troika.mk
+++ omni_troika.mk
@@ -1,5 +1,4 @@
-#
-# Copyright 2020 The Android Open Source Project
+# Copyright 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,40 +13,16 @@
 # limitations under the License.
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := troika
+
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# A/B updater
-AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-    boot \
-    system \
-    vendor
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier
-
-# The following modules are included in debuggable builds only.
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl \
-    update_engine_client
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    libgptutils \
-    libz \
-    libcutils
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/motorola/troika/device.mk)
 
 PRODUCT_PACKAGES += \
     charger_res_images \
@@ -57,9 +32,21 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     system/timezone/output_data/iana/tzdata:recovery/root/system_root/usr/share/zoneinfo/tzdata
 
-# Device identifier. This must come after all inclusions
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := troika
 PRODUCT_NAME := omni_troika
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := motorola one action
+PRODUCT_MODEL := moto one
 PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=troika \
+    BUILD_PRODUCT=troika \
+    TARGET_DEVICE=troika
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.product.model \
+    ro.product.device \
+    ro.product.name \
+    ro.build.product \
+    ro.display.series
